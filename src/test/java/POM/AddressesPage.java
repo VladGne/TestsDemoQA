2d3b0f9532b6b8a7970cb440f4a8ebf49148ceec
Vlad Gneushev <guinwow@gmail.com>
2018-11-19 13:49:20 +0400

    [+] Page factory for addresses validation

--- src/test/java/POM/AddressesPage.java
+++ src/test/java/POM/AddressesPage.java
@@ -1,5 +1,6 @@
 package POM;
 
+import Models.User;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,7 +21,44 @@ public class AddressesPage extends BasePage{
     @FindBy(xpath = "//span[@class='address_name']")
     private List<WebElement> fullName;
 
-    private boolean checkFullName(List<WebElement> fullName, String expectedFirstName, String expectedSecondName){
+    @FindBy(className = "building")
+    private WebElement addressButton;
+
+    @FindBy(className = "company")
+    private WebElement companyField;
+
+    @FindBy(className = "address1")
+    private WebElement address1Field;
+
+    @FindBy(className = "address2")
+    private WebElement address2Field;
+
+    @FindBy(className = "phone")
+    private WebElement homePhoneField;
+
+    @FindBy(className = "phone_mobile")
+    private WebElement mobilePhoneField;
+
+    @FindBy(id = "email")
+    private WebElement emailTextbox;
+
+    @FindBy(id = "passwd")
+    private WebElement passwordTextbox;
+
+    @FindBy(id = "SubmitLogin")
+    private WebElement loginButton;
+
+    public void addressButtonClick(){
+        addressButton.click();
+    }
+
+    public void doLogin(String email, String password){
+        emailTextbox.sendKeys(email);
+        passwordTextbox.sendKeys(password);
+        loginButton.click();
+    }
+
+    public boolean checkFullName(String expectedFirstName, String expectedSecondName){
        String firstName = fullName.get(0).getText();
        String lastName = fullName.get(1).getText();
 
@@ -32,9 +70,24 @@ public class AddressesPage extends BasePage{
         return true;
     }
 
-    private boolean checkField(){
+    public boolean checkCompany(String expectedCompany){
+        return companyField.getText().equals(expectedCompany);
+    }
 
-        return true;
+    public boolean checkAdderss1(String expectedAddress1){
+        return address1Field.getText().equals(expectedAddress1);
+    }
+
+    public boolean checkAddress2(String expectedAddress2){
+        return address2Field.getText().equals(expectedAddress2);
+    }
+
+    public boolean checkHomePhone(String expectedPhone){
+        return homePhoneField.getText().equals(expectedPhone);
+    }
+
+    public boolean checkMobilePhone(String expectedPhone){
+        return mobilePhoneField.getText().equals(expectedPhone);
     }
 
 
