4e2657e8d7105477cc86f778d0ded9ec2d87110d
Vlad Gneushev <guinwow@gmail.com>
2018-11-19 19:00:44 +0400

    [...] use general method for checking form

--- src/test/java/com/org/MavenTests/PersonalInfoTests.java
+++ src/test/java/com/org/MavenTests/PersonalInfoTests.java
@@ -2,7 +2,6 @@ package com.org.MavenTests;
 
 import Models.User;
 import POM.PersonalPage;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.log4testng.Logger;
@@ -34,59 +33,8 @@ public class PersonalInfoTests extends TestBase{
         logger.info("Navigate to personal page");
         personalPage.navigateToPersonalInfo();
 
-        logger.info("Check gender");
-        if(!personalPage.checkMaleGender()){
-            logger.error("Gender validation error");
-            softAssertion.fail("Gender validation error");
-        }
-
-        logger.info("Check first name");
-        if(!personalPage.checkFirstName(user.getFistName())){
-            logger.error("First name validation error");
-            softAssertion.fail("First name validation error");
-        }
-
-        logger.info("Check last name");
-        if(!personalPage.checkLastName(user.getLastName())){
-            logger.error("Last name validation error");
-            softAssertion.fail("Last name validation error");
-        }
-
-        logger.info("Check email");
-        if(!personalPage.checkEmail(user.getEmail())){
-            logger.error("Email validation error");
-            softAssertion.fail("Email validation error");
-        }
-
-        logger.info("Check birth day");
-        if(!personalPage.checkBithDay(user.getDayBirth())){
-            logger.error("Birth day validation error");
-            softAssertion.fail("Birth day validation error");
-        }
-
-        logger.info("Check birth month");
-        if(!personalPage.checkBithMonth(user.getMonthBirth())){
-            logger.error("Birth month validation error");
-            softAssertion.fail("Birth month validation error");
-        }
-
-        logger.info("Check birth year");
-        if(!personalPage.checkBithYear(user.getYearBirth())){
-            logger.error("Birth year validation error");
-            softAssertion.fail("Birth year validation error");
-        }
-
-        logger.info("Check news");
-        if(!personalPage.checkNews()){
-            logger.error("News validation error");
-            softAssertion.fail("News validation error");
-        }
-
-        logger.info("Check options");
-        if(!personalPage.checkOptions()){
-            logger.error("Option validation error");
-            softAssertion.fail("Option validation error");
-        }
+        logger.info("Check personal information form");
+        personalPage.checkPersonalInfoForm(user, logger);
 
         softAssertion.assertAll();
         //profilePage.click(By.className(BasePage.LOGIN_BUTTON_LOCATOR));
