b7078491ca8cbc4720e1c9ad9d97233534d95ff6
Vlad Gneushev <guinwow@gmail.com>
2018-11-19 19:00:18 +0400

    [+] general method for checking form

--- src/test/java/POM/AddressesPage.java
+++ src/test/java/POM/AddressesPage.java
@@ -4,6 +4,7 @@ import Models.User;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.log4testng.Logger;
 
 import java.util.List;
 
@@ -73,66 +74,97 @@ public class AddressesPage extends BasePage{
         loginButton.click();
     }
 
-    public void checkFullName(String expectedFirstName, String expectedSecondName){
+    private void checkFullName(String expectedFirstName, String expectedLastName){
        String firstName = fullName.get(0).getText();
        String lastName = fullName.get(1).getText();
 
        if(!firstName.equals(expectedFirstName))
-           softAssertion.fail("First name doesn't match");
+           softAssertion.fail(String.format("First name doesn't match: actual - %s, expected - %s", firstName, expectedFirstName));
 
-       if(!lastName.equals(expectedSecondName))
-           softAssertion.fail("Last name doesn't match");
+       if(!lastName.equals(expectedLastName))
+           softAssertion.fail(String.format("Last name doesn't match: actual - %s, expected - %s", lastName, expectedLastName));
     }
 
-    public void checkCountry(User.Country expectedCountry){
+    private void checkCountry(User.Country expectedCountry){
 
         if(countryField.getText().equals(expectedCountry.toString()))
-            softAssertion.fail("Country doesn't match");
+            softAssertion.fail(String.format("Country doesn't match: actual - %s, expected - %s",countryField.getText(), expectedCountry.toString()));
     }
 
-    public void checkCity(String expectedCity){
+    private void checkCity(String expectedCity){
         String[] currentCity = city.getText().split(",");
         if (!currentCity[0].equals(expectedCity))
-            softAssertion.fail("City doesn't match");
+            softAssertion.fail(String.format("City doesn't match: actual - %s, expected - %s", currentCity[0], expectedCity));
     }
 
-    public void checkState(User.State expectedState){
+    private void checkState(User.State expectedState){
         if (!state.getText().equals(expectedState.toString()))
-            softAssertion.fail("State doesn't match");
+            softAssertion.fail(String.format("State doesn't match: actual - %s, expected - %s", state.getText(), expectedState.toString()));
     }
 
-    public void checkPostCode(String expectedPostcode){
+    private void checkPostCode(String expectedPostcode){
         if (postCode.getText().equals(expectedPostcode))
-            softAssertion.fail("Postcode doesn't match");
+            softAssertion.fail(String.format("Postcode doesn't match: actual - %s, expected - %s",postCode.getText(), expectedPostcode));
     }
 
-    public void checkCompany(String expectedCompany){
+    private void checkCompany(String expectedCompany){
         if(companyField.getText().equals(expectedCompany))
-            softAssertion.fail("Company doesn't match");
+            softAssertion.fail(String.format("Company doesn't match: actual - %s, expected - %s", companyField.getText(), expectedCompany));
     }
 
-    public void checkAdderss1(String expectedAddress1){
+    private void checkAdderss1(String expectedAddress1){
         if(address1Field.getText().equals(expectedAddress1))
-           softAssertion.fail("Address1 doesn't match");
+           softAssertion.fail(String.format("Address1 doesn't match: actual - %s, expected - %s", address1Field.getText(), expectedAddress1));
     }
 
-    public void checkAddress2(String expectedAddress2){
+    private void checkAddress2(String expectedAddress2){
         if(address2Field.getText().equals(expectedAddress2))
-            softAssertion.fail("Address2 doesn't match");
+            softAssertion.fail(String.format("Address2 doesn't match: actual - %s, expected - %s", address2Field.getText(), expectedAddress2));
     }
 
-    public void checkHomePhone(String expectedPhone){
+    private void checkHomePhone(String expectedPhone){
         if(homePhoneField.getText().equals(expectedPhone))
-            softAssertion.fail("Home phone doesn't match");
+            softAssertion.fail(String.format("Home phone doesn't match: actual - %s, expected - %s", homePhoneField.getText(), expectedPhone));
     }
 
-    public void checkMobilePhone(String expectedPhone){
+    private void checkMobilePhone(String expectedPhone){
         if(mobilePhoneField.getText().equals(expectedPhone))
-            softAssertion.fail("Mobile phone doesn't match");
+            softAssertion.fail(String.format("Mobile phone doesn't match: actual - %s, expected - %s", mobilePhoneField.getText(), expectedPhone));
     }
 
+    public void checkForm(User user, Logger logger){
+        logger.info("Check full name");
+        checkFullName(user.getFistName(),user.getLastName());
 
-    public AddressesPage(WebDriver driver) {
+        logger.info("Check company");
+        checkCompany(user.getCompany());
+
+        logger.info("Check address1");
+        checkAdderss1(user.getAddress());
+
+        logger.info("Check address2");
+        checkAddress2(user.getAddress2());
+
+        logger.info("Check home phone");
+        checkHomePhone(user.getHomePhone());
+
+        logger.info("Check mobile phone");
+        checkMobilePhone(user.getMobilePhone());
+
+        logger.info("Check country");
+        checkCountry(user.getCountry());
+
+        logger.info("Check city");
+        checkCity(user.getCity());
+
+        logger.info("Check state");
+        checkState(user.getState());
+
+        logger.info("Check Postcode");
+        checkPostCode(user.getZipCode());
+    }
+
+    private AddressesPage(WebDriver driver) {
         super(driver);
     }
 
