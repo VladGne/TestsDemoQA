5c1d9396aaaa00c8a31cab3ed4510008f7256e40
Vlad Gneushev <guinwow@gmail.com>
2018-11-19 15:20:41 +0400

    [...] modify methods logic

--- src/test/java/POM/AddressesPage.java
+++ src/test/java/POM/AddressesPage.java
@@ -21,22 +21,37 @@ public class AddressesPage extends BasePage{
     @FindBy(xpath = "//span[@class='address_name']")
     private List<WebElement> fullName;
 
-    @FindBy(className = "building")
+    @FindBy(xpath = "//ul[@class='last_item item box']//li[5]")
+    private List<WebElement> addressesField;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[5]/span[1]")
+    private WebElement city;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[5]/span[2]")
+    private WebElement state;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[5]/span[3]")
+    private WebElement postCode;
+
+    @FindBy(xpath = "//ul[@class='last_item item box']//li[6]")
+    private WebElement countryField;
+
+    @FindBy(className = "icon-building")
     private WebElement addressButton;
 
-    @FindBy(className = "company")
+    @FindBy(className = "address_company")
     private WebElement companyField;
 
-    @FindBy(className = "address1")
+    @FindBy(className = "address_address1")
     private WebElement address1Field;
 
-    @FindBy(className = "address2")
+    @FindBy(className = "address_address2")
     private WebElement address2Field;
 
-    @FindBy(className = "phone")
+    @FindBy(className = "address_phone")
     private WebElement homePhoneField;
 
-    @FindBy(className = "phone_mobile")
+    @FindBy(className = "address_phone_mobile")
     private WebElement mobilePhoneField;
 
     @FindBy(id = "email")
@@ -58,40 +73,72 @@ public class AddressesPage extends BasePage{
         loginButton.click();
     }
 
-    public boolean checkFullName(String expectedFirstName, String expectedSecondName){
+    public void checkFullName(String expectedFirstName, String expectedSecondName){
        String firstName = fullName.get(0).getText();
        String lastName = fullName.get(1).getText();
 
-       if (! firstName.equals(expectedFirstName))
-           return false;
-       if (lastName.equals(expectedSecondName))
-           return false;
+       if(!firstName.equals(expectedFirstName))
+           softAssertion.fail("First name doesn't match");
 
-        return true;
+       if(!lastName.equals(expectedSecondName))
+           softAssertion.fail("Last name doesn't match");
     }
 
-    public boolean checkCompany(String expectedCompany){
-        return companyField.getText().equals(expectedCompany);
+    public void checkCountry(User.Country expectedCountry){
+
+        if(countryField.getText().equals(expectedCountry.toString()))
+            softAssertion.fail("Country doesn't match");
     }
 
-    public boolean checkAdderss1(String expectedAddress1){
-        return address1Field.getText().equals(expectedAddress1);
+    public void checkCity(String expectedCity){
+        String[] currentCity = city.getText().split(",");
+        if (!currentCity[0].equals(expectedCity))
+            softAssertion.fail("City doesn't match");
     }
 
-    public boolean checkAddress2(String expectedAddress2){
-        return address2Field.getText().equals(expectedAddress2);
+    public void checkState(User.State expectedState){
+        if (!state.getText().equals(expectedState.toString()))
+            softAssertion.fail("State doesn't match");
     }
 
-    public boolean checkHomePhone(String expectedPhone){
-        return homePhoneField.getText().equals(expectedPhone);
+    public void checkPostCode(String expectedPostcode){
+        if (postCode.getText().equals(expectedPostcode))
+            softAssertion.fail("Postcode doesn't match");
     }
 
-    public boolean checkMobilePhone(String expectedPhone){
-        return mobilePhoneField.getText().equals(expectedPhone);
+    public void checkCompany(String expectedCompany){
+        if(companyField.getText().equals(expectedCompany))
+            softAssertion.fail("Company doesn't match");
+    }
+
+    public void checkAdderss1(String expectedAddress1){
+        if(address1Field.getText().equals(expectedAddress1))
+           softAssertion.fail("Address1 doesn't match");
+    }
+
+    public void checkAddress2(String expectedAddress2){
+        if(address2Field.getText().equals(expectedAddress2))
+            softAssertion.fail("Address2 doesn't match");
+    }
+
+    public void checkHomePhone(String expectedPhone){
+        if(homePhoneField.getText().equals(expectedPhone))
+            softAssertion.fail("Home phone doesn't match");
+    }
+
+    public void checkMobilePhone(String expectedPhone){
+        if(mobilePhoneField.getText().equals(expectedPhone))
+            softAssertion.fail("Mobile phone doesn't match");
     }
 
 
     public AddressesPage(WebDriver driver) {
         super(driver);
+    }
+
+    public static AddressesPage open(WebDriver driver) {
+        final String LoginPageURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+        driver.navigate().to(LoginPageURL);
+        return new AddressesPage(driver);
     }
 }
