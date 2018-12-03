c821eddfd71bb6cf02fec9c87c4f761ab9bb1b76
Vlad Gneushev <guinwow@gmail.com>
2018-12-03 20:17:53 +0400

    [+] check visibility elements method

--- src/test/java/com/org/MavenTests/TestBase.java
+++ src/test/java/com/org/MavenTests/TestBase.java
@@ -1,13 +1,21 @@
 package com.org.MavenTests;
 
 import POM.BasePage;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.pagefactory.ByAll;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import org.testng.log4testng.Logger;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class TestBase {
 
@@ -28,5 +36,35 @@ public class TestBase {
         //driver.close();
         //driver.quit();
         logger.info("Test end!");
+    }
+
+    private List<WebElement> getAllElements(WebDriver driver){
+        List<WebElement> allElementsList = new LinkedList<>(driver.findElements(By.cssSelector("*")));
+        return allElementsList;
+    }
+
+    private void checkVisibility(List<WebElement> webElementsList){
+
+        List<WebElement> hiddenElements = new LinkedList<>();
+
+        for (WebElement element: webElementsList)
+            if (!element.isDisplayed())
+                hiddenElements.add(element);
+
+        if (hiddenElements.size()>0){
+            logger.error("Number of hidden elements: " + hiddenElements.size());
+            for (WebElement element: webElementsList)
+                logger.error("Hidden element: " + element.getText());
+            Assert.fail();
+        }
+
+        else {
+            logger.info("Number of hidden elements: 0");
+        }
+    }
+
+    public void checkPageElementsVisibility(WebDriver driver){
+        List<WebElement> pageElements = getAllElements(driver);
+        checkVisibility(pageElements);
     }
 }
