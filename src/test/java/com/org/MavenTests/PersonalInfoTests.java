3acc77f85f0f6df02b8dc76d300bb9c0757b47c2
Vlad Gneushev <guinwow@gmail.com>
2018-12-03 20:18:38 +0400

    [+] checking elements visibility

--- src/test/java/com/org/MavenTests/PersonalInfoTests.java
+++ src/test/java/com/org/MavenTests/PersonalInfoTests.java
@@ -2,9 +2,9 @@ package com.org.MavenTests;
 
 import Models.User;
 import POM.PersonalPage;
+import org.apache.log4j.Logger;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.log4testng.Logger;
 
 public class PersonalInfoTests extends TestBase{
 
@@ -17,6 +17,25 @@ public class PersonalInfoTests extends TestBase{
         validUserData[0] = new User();
         validUserData[0].setEmail("test1@test.com1");
         return validUserData;
+    }
+
+    @Test(priority = 0, dataProvider="validUserData")
+    public void checkVisibility(User user){
+
+        logger.info("\n --- Check personal info page elements visibility test start ---\n");
+
+        logger.info("Navigate to login page");
+        PersonalPage personalPage = PersonalPage.open(driver);
+
+        logger.info("Login");
+        personalPage.doLogin(user.getEmail(),user.getPassword());
+
+        logger.info("Navigate to personal page");
+        personalPage.navigateToPersonalInfo();
+
+        checkPageElementsVisibility(driver);
+
+        logger.info("\n --- Check personal info page elements visibility test end ---\n");
     }
 
     @Test(priority = 1, dataProvider="validUserData")
