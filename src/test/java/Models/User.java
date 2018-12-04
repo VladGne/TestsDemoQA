7ec9ccdfb8f24fd375da25a7d1390d9a3a8b72da
Vlad Gneushev <guinwow@gmail.com>
2018-12-04 18:09:19 +0400

    [+] lombok getters & setters for all fields

--- src/test/java/Models/User.java
+++ src/test/java/Models/User.java
@@ -1,29 +1,32 @@
 package Models;
 
-public class User {
-	private String email;
-	private String fistName;
-	private String lastName;
-	private String company;
-	private String address;
-	private String address2; // Additional address information
-	private String city;
-	private String zipCode;
-	private String additionInformation;
-	private String homePhone;
-	private String mobilePhone;
-	private String addressAlias;
-	private String gender;
-	private String password;
-	private String dayBirth;
-	private String yearBirth;
+import lombok.Getter;
+import lombok.Setter;
 
-	private State state;
-	private Country country;
-	private MonthBirth monthBirth;
+public class User {
+
+	@Getter @Setter	private String email;
+	@Getter @Setter private String fistName;
+	@Getter @Setter private String lastName;
+	@Getter @Setter private String company;
+	@Getter @Setter private String address;
+	@Getter @Setter private String address2; // Additional address information
+	@Getter @Setter private String city;
+	@Getter @Setter private String zipCode;
+	@Getter @Setter private String additionInformation;
+	@Getter @Setter private String homePhone;
+	@Getter @Setter private String mobilePhone;
+	@Getter @Setter private String addressAlias;
+	@Getter @Setter private String gender;
+	@Getter @Setter private String password;
+	@Getter @Setter private String dayBirth;
+	@Getter @Setter private String yearBirth;
+	@Getter @Setter private State state;
+	@Getter @Setter private Country country;
+	@Getter @Setter private MonthBirth monthBirth;
 
 	public User(){
-		final String email = "test1@test.com1";								//test1@test.com1 - for login
+		final String email = "test2@test.com2";								//test1@test.com1 - for login
 		final String gender = "Male";
 		final String fistName = "TestFistName";
 		final String lastName = "TestLastName";
@@ -107,13 +110,9 @@ public class User {
 		Colorado(6),
 		Connecticut(7);
 
-		int value;
+		@Getter int value;
 		State(int value){
 				this.value = value;
-		}
-
-		public int getValue() {
-			return this.value;
 		}
 	}
 	
@@ -131,144 +130,16 @@ public class User {
 		November (11),
 		December (12);
 
-		int value;
+		@Getter int value;
 		MonthBirth(int value){
 			this.value = value;
-		}
-
-		public int getValue() {
-			return this.value;
 		}
 	}
 
 	public static enum Country{
 		UnitedStates(21);
-		int value;
+
+		@Getter int value;
 		Country(int value) { this.value = value;}
-
-		public int getValue() {
-			return this.value;
-		}
-	}
-	
-	public String getDayBirth() {
-		return dayBirth;
-	}
-	public void setDayBirth(String dayBirth) {
-		this.dayBirth = dayBirth;
-	}
-	public String getYearBirth() {
-		return yearBirth;
-	}
-	public void setYearBirth(String yearBirth) {
-		this.yearBirth = yearBirth;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}	
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getFistName() {
-		return fistName;
-	}
-	public void setFistName(String fistName) {
-		this.fistName = fistName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getCompany() {
-		return company;
-	}
-	public void setCompany(String company) {
-		this.company = company;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getAddress2() {
-		return address2;
-	}
-	public void setAddress2(String address2) {
-		this.address2 = address2;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getZipCode() {
-		return zipCode;
-	}
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-	public String getAdditionInformation() {
-		return additionInformation;
-	}
-	public void setAdditionInformation(String additionInformation) {
-		this.additionInformation = additionInformation;
-	}
-	public String getHomePhone() {
-		return homePhone;
-	}
-	public void setHomePhone(String homePhone) {
-		this.homePhone = homePhone;
-	}
-	public String getMobilePhone() {
-		return mobilePhone;
-	}
-	public void setMobilePhone(String mobilePhone) {
-		this.mobilePhone = mobilePhone;
-	}
-	public String getAddressAlias() {
-		return addressAlias;
-	}
-	public void setAddressAlias(String addressAlias) {
-		this.addressAlias = addressAlias;
-	}
-
-	public State getState() {
-		return state;
-	}
-
-	public void setState(State state) {
-		this.state = state;
-	}
-
-	public Country getCountry() {
-		return country;
-	}
-
-	public void setCountry(Country country) {
-		this.country = country;
-	}
-
-	public MonthBirth getMonthBirth() {
-		return monthBirth;
-	}
-
-	public void setMonthBirth(MonthBirth monthBirth) {
-		this.monthBirth = monthBirth;
 	}
 }
