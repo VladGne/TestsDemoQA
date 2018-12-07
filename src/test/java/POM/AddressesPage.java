c5f3458930c94cda575ea9a2cee40261c9007048
Vlad Gneushev <guinwow@gmail.com>
2018-12-07 20:08:12 +0400

    [-] useless check methods

--- src/test/java/POM/AddressesPage.java
+++ src/test/java/POM/AddressesPage.java
@@ -5,10 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-
 import java.util.List;
 
 public class AddressesPage extends BasePage{
+
+    final String LoginPageURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
 
     @FindBy(xpath = "//span[@class='address_name']")
     private List<WebElement> fullName;
@@ -28,9 +29,6 @@ public class AddressesPage extends BasePage{
     @FindBy(xpath = "//ul[@class='last_item item box']//li[6]")
     private WebElement countryField;
 
-    @FindBy(className = "icon-building")
-    private WebElement addressButton;
-
     @FindBy(className = "address_company")
     private WebElement companyField;
 
@@ -46,8 +44,9 @@ public class AddressesPage extends BasePage{
     @FindBy(className = "address_phone_mobile")
     private WebElement mobilePhoneField;
 
-    public void addressButtonClick(){
-        addressButton.click();
+    public void addressButtonClick(WebDriver driver){
+        AccountPage accountPage = new AccountPage(driver);
+        accountPage.addressButtonClick();
     }
 
     public User getActualUserData(){
@@ -60,7 +59,7 @@ public class AddressesPage extends BasePage{
         String[] currentCity = city.getText().split(",");
         actualUser.setCity(currentCity[0]);
         actualUser.setState(User.State.valueOf(state.getText()));
-        actualUser.setZipCode(postCode.getText() + "12321");
+        actualUser.setZipCode(postCode.getText());
         actualUser.setCompany(companyField.getText());
         actualUser.setAddress(address1Field.getText());
         actualUser.setAddress2(address2Field.getText());
@@ -70,105 +69,8 @@ public class AddressesPage extends BasePage{
         return actualUser;
     }
 
-    private void checkFullName(String expectedFirstName, String expectedLastName){
-       String firstName = fullName.get(0).getText();
-       String lastName = fullName.get(1).getText();
-
-//       if(!firstName.equals(expectedFirstName))
-//           softAssertion.fail(String.format("First name doesn't match: actual - %s, expected - %s", firstName, expectedFirstName));
-//
-//       if(!lastName.equals(expectedLastName))
-//           softAssertion.fail(String.format("Last name doesn't match: actual - %s, expected - %s", lastName, expectedLastName));
-    }
-
-    private void checkCountry(User.Country expectedCountry){
-
-//        if(countryField.getText().equals(expectedCountry.toString()))
-//            softAssertion.fail(String.format("Country doesn't match: actual - %s, expected - %s",countryField.getText(), expectedCountry.toString()));
-    }
-
-    private void checkCity(String expectedCity){
-        String[] currentCity = city.getText().split(",");
-//        if (!currentCity[0].equals(expectedCity))
-//            softAssertion.fail(String.format("City doesn't match: actual - %s, expected - %s", currentCity[0], expectedCity));
-    }
-
-    private void checkState(User.State expectedState){
-//        if (!state.getText().equals(expectedState.toString()))
-//            softAssertion.fail(String.format("State doesn't match: actual - %s, expected - %s", state.getText(), expectedState.toString()));
-    }
-
-    private void checkPostCode(String expectedPostcode){
-//        if (postCode.getText().equals(expectedPostcode))
-//            softAssertion.fail(String.format("Postcode doesn't match: actual - %s, expected - %s",postCode.getText(), expectedPostcode));
-    }
-
-    private void checkCompany(String expectedCompany){
-//        if(companyField.getText().equals(expectedCompany))
-//            softAssertion.fail(String.format("Company doesn't match: actual - %s, expected - %s", companyField.getText(), expectedCompany));
-    }
-
-    private void checkAdderss1(String expectedAddress1){
-//        if(address1Field.getText().equals(expectedAddress1))
-//           softAssertion.fail(String.format("Address1 doesn't match: actual - %s, expected - %s", address1Field.getText(), expectedAddress1));
-    }
-
-    private void checkAddress2(String expectedAddress2){
-//        if(address2Field.getText().equals(expectedAddress2))
-//            softAssertion.fail(String.format("Address2 doesn't match: actual - %s, expected - %s", address2Field.getText(), expectedAddress2));
-    }
-
-    private void checkHomePhone(String expectedPhone){
-//        if(homePhoneField.getText().equals(expectedPhone))
-//            softAssertion.fail(String.format("Home phone doesn't match: actual - %s, expected - %s", homePhoneField.getText(), expectedPhone));
-    }
-
-    private void checkMobilePhone(String expectedPhone){
-//        if(mobilePhoneField.getText().equals(expectedPhone))
-//            softAssertion.fail(String.format("Mobile phone doesn't match: actual - %s, expected - %s", mobilePhoneField.getText(), expectedPhone));
-    }
-
-    public void checkForm(User user){
-        logger.debug("Check full name");
-        checkFullName(user.getFistName(),user.getLastName());
-
-        logger.debug("Check company");
-        checkCompany(user.getCompany());
-
-        logger.debug("Check address1");
-        checkAdderss1(user.getAddress());
-
-        logger.debug("Check address2");
-        checkAddress2(user.getAddress2());
-
-        logger.debug("Check home phone");
-        checkHomePhone(user.getHomePhone());
-
-        logger.debug("Check mobile phone");
-        checkMobilePhone(user.getMobilePhone());
-
-        logger.debug("Check country");
-        checkCountry(user.getCountry());
-
-        logger.debug("Check city");
-        checkCity(user.getCity());
-
-        logger.debug("Check state");
-        checkState(user.getState());
-
-        logger.debug("Check Postcode");
-        checkPostCode(user.getZipCode());
-    }
-
-
-
-    private AddressesPage(WebDriver driver) {
+    public AddressesPage(WebDriver driver) {
         super(driver);
-    }
-
-    static public  AddressesPage open() {
-        final String LoginPageURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
-        driver.navigate().to(LoginPageURL);
-        return new AddressesPage(driver);
+        BasePage.navigate(driver, LoginPageURL);
     }
 }
--- src/test/java/POM/PersonalPage.java
+++ src/test/java/POM/PersonalPage.java
@@ -5,18 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
 public class PersonalPage extends BasePage{
 
-    private PersonalPage(WebDriver driver) {
-        super(driver);
-    }
+    private final String LOGIN_PAGE_URL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
 
-    @FindBy(className = "icon-user")
-    private WebElement personalInfoButton;
+    public PersonalPage(WebDriver driver) {
+        super(driver);
+        BasePage.navigate(driver, LOGIN_PAGE_URL);
+    }
 
     @FindBy(className = "std")
     private WebElement personalInfoForm;
@@ -51,14 +50,10 @@ public class PersonalPage extends BasePage{
     @FindBy(id = "optin")
     private WebElement optionCheckBox;
 
-    public void navigateToPersonalInfo(){
-        personalInfoButton.click();
-    }
+    public void navigateToPersonalInfo(WebDriver driver){
+        AccountPage accountPage = new AccountPage(driver);
+        accountPage.personalInfoButtonClick();
 
-    public void waitForPersonalInfoForm(){
-
-        WebDriverWait wait = new WebDriverWait(driver, BasePage.waiterTime);
-        WebElement form = wait.until((WebDriver d) -> personalInfoForm) ;
     }
 
     public User getActualUserData(){
@@ -84,104 +79,5 @@ public class PersonalPage extends BasePage{
         actualUser.setOptions(optionCheckBox.isSelected());
 
         return actualUser;
-    }
-
-    private boolean checkMaleGender(){
-            return maleCheckBox.isSelected();
-    }
-
-    public boolean checkFemaleGender(){
-        return femaleCheckBox.isSelected();
-    }
-
-    private void checkFirstName(String expectedFirstName){
-        String actualName = firstName.getAttribute("value");
-        if(!actualName.equals(expectedFirstName));
-          //  softAssertion.fail(String.format("First name validation error: actual - %s, expected - %s",actualName, expectedFirstName ));
-    }
-
-    private void checkLastName(String expectedLastName){
-        String actualName = lastName.getAttribute("value");
-        if(!actualName.equals(expectedLastName));
-           // softAssertion.fail(String.format("Last name validation error: actual - %s, expected - %s",actualName, expectedLastName ));
-    }
-
-    private void checkEmail (String expectedEmail){
-        String actualEmail = email.getAttribute("value");
-        if(!actualEmail.equals(expectedEmail));
-           // softAssertion.fail(String.format("Email validation error: actual - %s, expected - %s", actualEmail, expectedEmail ));
-    }
-
-    private void checkBithDay(String expectedDay){
-
-        List<WebElement> daysList = new Select(days).getAllSelectedOptions();
-        String[] selectedDay = daysList.get(0).getText().split(" ");
-        if(selectedDay[0].equals(expectedDay));
-            //softAssertion.fail(String.format("Birth day validation error: actual - %s, expected - %s", selectedDay[0], expectedDay ));
-    }
-
-    private void checkBithMonth(User.MonthBirth expectedMonth){
-
-        List<WebElement> monthList = new Select(month).getAllSelectedOptions();
-        String[] selectedMonth = monthList.get(0).getText().split(" ");
-        if(selectedMonth[0].equals(expectedMonth.toString()));
-            //softAssertion.fail(String.format("Birth month validation error: actual - %s, expected - %s", selectedMonth[0], expectedMonth.toString() ));
-    }
-
-    private void checkBithYear(String expectedYear){
-
-        List<WebElement> yearList = new Select(year).getAllSelectedOptions();
-        String[] selectedYear = yearList.get(0).getText().split(" ");
-        if(selectedYear[0].equals(expectedYear));
-            //softAssertion.fail(String.format("Birth year validation error: actual - %s, expected - %s", selectedYear[0], expectedYear ));
-    }
-
-    private boolean checkNews(){
-        return newsCheckBox.isSelected();
-    }
-
-    private boolean checkOptions(){
-        return optionCheckBox.isSelected();
-    }
-
-    public static PersonalPage open() {
-        final String LoginPageURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
-        driver.navigate().to(LoginPageURL);
-        return new PersonalPage(driver);
-    }
-
-    public void checkPersonalInfoForm(User user){
-        logger.info("Check gender");
-        if(!checkMaleGender()){
-            //softAssertion.fail("Gender validation error: male gender isn't selected");
-        }
-
-        logger.info("Check first name");
-        checkFirstName(user.getFistName());
-
-        logger.info("Check last name");
-        checkLastName(user.getLastName());
-
-        logger.info("Check email");
-        checkEmail(user.getEmail());
-
-        logger.info("Check birth day");
-        checkBithDay(user.getDayBirth());
-
-        logger.info("Check birth month");
-        checkBithMonth(user.getMonthBirth());
-
-        logger.info("Check birth year");
-        checkBithYear(user.getYearBirth());
-
-        logger.info("Check news");
-        if(!checkNews()){
-           // softAssertion.fail("News validation error: news checkbox isn't selected");
-        }
-
-        logger.info("Check options");
-        if(!checkOptions()){
-          //  softAssertion.fail("Option validation error: options checkbox isn't selected");
-        }
     }
 }
