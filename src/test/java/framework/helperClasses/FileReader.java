19bb0949361df98e73a3246433c88ff96ad0da29
Vlad Gneushev <guinwow@gmail.com>
2018-12-11 20:12:47 +0400

    [...] read json [+] get data method

--- src/test/java/framework/helperClasses/FileReader.java
+++ src/test/java/framework/helperClasses/FileReader.java
@@ -3,26 +3,67 @@ package framework.helperClasses;
 import framework.models.User;
 import org.codehaus.jackson.map.ObjectMapper;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Properties;
 
 public class FileReader {
 
-    public static User readUserDataFrom(String filePath){
-        User user = new User();
+//    Map<String, Object> data;
+//
+//    public void readUserDataFrom(String filePath){
+//        //User user = new User();
+//        data = new HashMap<String, Object>();
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        try {
+//            // Convert JSON string from file to Object
+//            //user = mapper.readValue(new File(filePath), User.class);
+//
+//            byte[] mapData = Files.readAllBytes(Paths.get(filePath));
+//
+//            data =  mapper.readValue(allData, User.class);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-        ObjectMapper mapper = new ObjectMapper();
+    Collection<Object> users;
+
+    public void processDataFile( String filePath ){
+        users = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user[];
         try {
-            // Convert JSON string from file to Object
-            user = mapper.readValue(new File(filePath), User.class);
+            byte[] validUsers = Files.readAllBytes(Paths.get(filePath));
+            //data =  objectMapper.readValue(allData, User.class);
+            //Object obj = objectMapper.readValue( new File( filePath ), User.class );
+            users.add( objectMapper.readValue(validUsers, User[].class));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        return user;
+    public Object[][] getData() {
+
+        Object[][] data = new Object[ users.size() ][ 1 ];
+
+        Iterator<Object> iterator = users.iterator();
+
+        int i = 0;
+        while( iterator.hasNext() ) {
+            data[ i ][ 0 ] = iterator.next();
+            i++;
+        }
+
+        return data;
     }
 
     public static Properties readProperties(){
