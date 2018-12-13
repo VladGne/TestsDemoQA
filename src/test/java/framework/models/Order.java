f4a16b1a363914f5f508b7797320391b7de5d8ae
Vlad Gneushev <guinwow@gmail.com>
2018-12-13 19:42:28 +0400

    [+] order model

---
+++ src/test/java/framework/models/Order.java
@@ -0,0 +1,27 @@
+package framework.models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+
+@Data
+public class Order extends User {
+    private Category category;
+    private int quantity = 1;
+    private Size size = Size.S;
+    private String color;
+    private String price;
+
+    @Getter @ToString
+    public enum Size{
+        S,
+        M,
+        L
+    }
+
+    @Getter @ToString
+    public enum Category{
+        TOP,
+        DRESS
+    }
+}
