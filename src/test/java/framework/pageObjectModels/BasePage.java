8d67c27b3f6bc16ab427015b5b2c32177f9ef078
Vlad Gneushev <guinwow@gmail.com>
2018-12-14 19:48:45 +0400

    [+] market buttons

--- src/test/java/framework/pageObjectModels/BasePage.java
+++ src/test/java/framework/pageObjectModels/BasePage.java
@@ -1,5 +1,6 @@
 package framework.pageObjectModels;
 
+import lombok.SneakyThrows;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
@@ -16,104 +17,113 @@ import java.util.Properties;
 
 import static framework.helperClasses.FileReader.readProperties;
 
-public class BasePage{
+public class BasePage {
 
-	private static Properties prop = readProperties();
-	static int waiterTime = 10;
-	public static final String DRIVER_PATH = "src\\test\\resources\\chromedriver.exe";
-	Logger logger = LogManager.getLogger(this);
+    private static Properties prop = readProperties();
+    static int waiterTime = 10;
+    public static final String DRIVER_PATH = "src\\test\\resources\\chromedriver.exe";
+    Logger logger = LogManager.getLogger(this);
 
-	@FindBy(className = "logout")
-	private WebElement logoutButton;
+    @FindBy(className = "logout")
+    private WebElement logoutButton;
 
-	@FindBy(xpath = "//div[@class='alert alert-danger']//ol")
-	private WebElement alertList;
+    @FindBy(xpath = "//div[@class='alert alert-danger']//ol")
+    private WebElement alertList;
 
-	@FindBy(className = "//ul[@class='footer_links clearfix']//li[1]")
-	private WebElement backToAccountButton;
+    @FindBy(className = "//ul[@class='footer_links clearfix']//li[1]")
+    private WebElement backToAccountButton;
 
-	@FindBy(className = "header_user_info")
-	private WebElement userProfileButton;
+    @FindBy(className = "header_user_info")
+    private WebElement userProfileButton;
 
-	BasePage(WebDriver driver) {
-		PageFactory.initElements(driver, this);
-		final String BasePageURL = prop.getProperty("durl");
-		//driver.navigate().to(BasePageURL);
-	}
+    @FindBy(linkText = "T-SHIRTS")
+    private WebElement tShirtButton;
 
-	public BasePage() {
-	}
+    @FindBy(className = "sf-with-ul")
+    WebElement womenButton;
 
-	public void backToAccountButtonClick(){
-		backToAccountButton.click();
-	}
+    BasePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        final String BasePageURL = prop.getProperty("durl");
+        //driver.navigate().to(BasePageURL);
+    }
 
-	public void userProfileButtonClick(){
-		userProfileButton.click();
-	}
+    public BasePage() {
+    }
 
-	public void waitSuccessfulMessage(WebDriver driver){
-		WebDriverWait wait = new WebDriverWait(driver, BasePage.waiterTime);
-		WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("identity")));
-	}
+    public void backToAccountButtonClick() {
+        backToAccountButton.click();
+    }
 
-	public void doLogin(String email, String password, WebDriver driver){
-		LoginPage loginPage = new LoginPage(driver);
-		loginPage.inputEmail(email);
-		loginPage.inputPassword(password);
-		loginPage.loginButtonClick();
-	}
+    public void userProfileButtonClick() {
+        userProfileButton.click();
+    }
 
-	public void doLogout(){
-		logoutButton.click();
-	}
+    public void womenButtonClick() {
+        womenButton.click();
+    }
+public void waitSuccessfulMessage(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, BasePage.waiterTime);
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("identity")));
+    }
 
-	static void navigate(WebDriver driver, String Url){
-		driver.navigate().to(Url);
-		//return new BasePage(driver);
-	}
+    public void doLogin(String email, String password, WebDriver driver) {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.inputEmail(email);
+        loginPage.inputPassword(password);
+        loginPage.loginButtonClick();
+    }
 
-	public void checkPageElementsVisibility(Class classObject, SoftAssert softAssert){
+    public void doLogout() {
+        logoutButton.click();
+    }
 
-		//ToDo : why isDisplayed work not correct?
+    static void navigate(WebDriver driver, String Url) {
+        driver.navigate().to(Url);
+        //return new BasePage(driver);
+    }
 
-		for (Field field : classObject.getDeclaredFields()) {
-			if (field.getType() == WebElement.class){
-				try {
-					field.setAccessible(true);
-					WebElement element = (WebElement) field.get(this);
-					logger.debug("Check visibility of " + field.getName());
-					if (!element.isEnabled())
-						softAssert.fail("Element isn't enabled - " + field.getName());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    public void tShirtButtonClick() {
+        tShirtButton.click();
+    }
 
-	public String[] getAlertList(){
-		return alertList.getText().split("\n");
-	}
+    @SneakyThrows
+    public void checkPageElementsVisibility(Class className, SoftAssert softAssert) {
+        //ToDo : why isDisplayed work not correct?
 
-	static class AccountPage{
+        for (Field field : className.getDeclaredFields()) {
+            if (field.getType() == WebElement.class) {
+                field.setAccessible(true);
+                WebElement element = (WebElement) field.get(this);
+                logger.debug("Check visibility of " + field.getName());
+                if (!element.isEnabled())
+                    softAssert.fail("Element isn't enabled - " + field.getName());
+            }
+        }
+    }
 
-		@FindBy(className = "icon-building")
-		private WebElement addressButton;
+    public String[] getAlertList() {
+        return alertList.getText().split("\n");
+    }
 
-		@FindBy(className = "icon-user")
-		private WebElement personalInfoButton;
+    static class AccountPage {
 
-		AccountPage(WebDriver driver) {
-			PageFactory.initElements(driver, this);
-		}
+        @FindBy(className = "icon-building")
+        private WebElement addressButton;
 
-		void addressButtonClick(){
-			addressButton.click();
-		}
+        @FindBy(className = "icon-user")
+        private WebElement personalInfoButton;
 
-		void personalInfoButtonClick(){
-			personalInfoButton.click();
-		}
-	}
+        AccountPage(WebDriver driver) {
+            PageFactory.initElements(driver, this);
+        }
+
+        void addressButtonClick() {
+            addressButton.click();
+        }
+
+        void personalInfoButtonClick() {
+            personalInfoButton.click();
+        }
+    }
 }
