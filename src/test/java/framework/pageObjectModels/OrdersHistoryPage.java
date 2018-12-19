9208852073dd79f7345569f7c2926d5ce2089e46
Vlad Gneushev <guinwow@gmail.com>
2018-12-19 19:20:27 +0400

    [+] order history tests
    [+] order history page model
    [+] test for history page to verify file downlad

--- src/test/java/framework/pageObjectModels/OrdersHistoryPage.java
+++ src/test/java/framework/pageObjectModels/OrdersHistoryPage.java
@@ -5,14 +5,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.asserts.SoftAssert;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
-public class OrdersHistoryPage {
+public class OrdersHistoryPage extends BasePage{
 
     public OrdersHistoryPage(WebDriver driver) {
         final String orderHistoryUrl = "http://automationpractice.com/index.php?controller=history";
@@ -28,22 +32,56 @@ public class OrdersHistoryPage {
     }
 
     @SneakyThrows
-    public void verifyFileDownloading(WebDriver driver, SoftAssert softAssert){
+    public void verifyFileDownloading(SoftAssert softAssert){
 
-        long currentFilesNumber = fileNumersInDownloads();
+        long currentFilesNumber = fileNumberInDownloads();
 
         downloadButtonClick();
 
         Thread.sleep(5000);
 
-        softAssert.assertTrue(currentFilesNumber + 1 == fileNumersInDownloads());
+        softAssert.assertTrue(currentFilesNumber + 1 == fileNumberInDownloads());
     }
 
     @SneakyThrows
-    private long fileNumersInDownloads(){
+    private long fileNumberInDownloads(){
         String filePath = String.format("C:\\Users\\%s\\Downloads\\",System.getProperty("user.name"));
         try (Stream<Path> files = Files.list(Paths.get(filePath))) {
             return files.count();
         }
+    }
+
+
+
+
+
+
+
+    private void waitForFileDownload(String expectedFileName, WebDriver driver){
+        int totalTimeoutInMillis = 10000;
+
+        FluentWait<WebDriver> wait = new FluentWait(driver)
+                .withTimeout(totalTimeoutInMillis, TimeUnit.MILLISECONDS)
+                .pollingEvery(200, TimeUnit.MILLISECONDS);
+        File fileToCheck = getDownloadsDirectory()
+                .resolve(expectedFileName)
+                .toFile();
+
+        wait.until((WebDriver wd) -> fileToCheck.exists());
+
+    }
+
+    private Path downloadsDirectory;
+    private synchronized Path getDownloadsDirectory(){
+
+        if(downloadsDirectory == null){
+
+            try {
+                downloadsDirectory = Files.createTempDirectory("selleniumdownloads_");
+            } catch (IOException ex) {
+                throw new RuntimeException("Failed to create temporary downloads directory");
+            }
+        }
+        return downloadsDirectory;
     }
 }
