ca55a549ed8cf7fdbef37d7529c766f98e41a50c
Vlad Gneushev <guinwow@gmail.com>
2018-12-21 18:33:01 +0400

    [...] update file waiter

--- src/test/java/framework/pageObjectModels/OrdersHistoryPage.java
+++ src/test/java/framework/pageObjectModels/OrdersHistoryPage.java
@@ -7,16 +7,15 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.asserts.SoftAssert;
-
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 public class OrdersHistoryPage extends BasePage{
+
+    final String FILEPATH = String.format("C:\\Users\\%s\\Downloads\\",System.getProperty("user.name"));
+
+    @FindBy (className="link-button")
+    WebElement downloadButton;
 
     public OrdersHistoryPage(WebDriver driver) {
         final String orderHistoryUrl = "http://automationpractice.com/index.php?controller=history";
@@ -24,64 +23,32 @@ public class OrdersHistoryPage extends BasePage{
         driver.navigate().to(orderHistoryUrl);
     }
 
-    @FindBy (className="icon-file-text")
-    WebElement downloadButton;
+    @SneakyThrows
+    public void verifyFileDownloading(SoftAssert softAssert, WebDriver driver){
 
-    public void downloadButtonClick(){
-        downloadButton.click();
+        String link = downloadButton.getAttribute("href");
+        driver.get(link);
+        waitForFileDownload(driver);
     }
 
-    @SneakyThrows
-    public void verifyFileDownloading(SoftAssert softAssert){
+    public boolean getAllFiles(){
+        File[] files = new File(FILEPATH).listFiles();
 
-        long currentFilesNumber = fileNumberInDownloads();
-
-        downloadButtonClick();
-
-        Thread.sleep(5000);
-
-        softAssert.assertTrue(currentFilesNumber + 1 == fileNumberInDownloads());
-    }
-
-    @SneakyThrows
-    private long fileNumberInDownloads(){
-        String filePath = String.format("C:\\Users\\%s\\Downloads\\",System.getProperty("user.name"));
-        try (Stream<Path> files = Files.list(Paths.get(filePath))) {
-            return files.count();
+        for (File file : files) {
+            if (file.isFile() && file.getName().contains(".pdf")) {
+                return true;
+            }
         }
+        return false;
     }
 
-
-
-
-
-
-
-    private void waitForFileDownload(String expectedFileName, WebDriver driver){
+    private void waitForFileDownload(WebDriver driver){
         int totalTimeoutInMillis = 10000;
 
         FluentWait<WebDriver> wait = new FluentWait(driver)
                 .withTimeout(totalTimeoutInMillis, TimeUnit.MILLISECONDS)
                 .pollingEvery(200, TimeUnit.MILLISECONDS);
-        File fileToCheck = getDownloadsDirectory()
-                .resolve(expectedFileName)
-                .toFile();
 
-        wait.until((WebDriver wd) -> fileToCheck.exists());
-
-    }
-
-    private Path downloadsDirectory;
-    private synchronized Path getDownloadsDirectory(){
-
-        if(downloadsDirectory == null){
-
-            try {
-                downloadsDirectory = Files.createTempDirectory("selleniumdownloads_");
-            } catch (IOException ex) {
-                throw new RuntimeException("Failed to create temporary downloads directory");
-            }
-        }
-        return downloadsDirectory;
+        wait.until((WebDriver wd) -> getAllFiles());
     }
 }
