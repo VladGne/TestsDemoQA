2a840a8102f17aeb10738a60906444d2cb5472a6
Vlad Gneushev <guinwow@gmail.com>
2018-12-21 18:35:21 +0400

    [+] complete order tests [+] verify order info in cart test [+] allure annotations

--- src/test/java/tests/MarketPageTests.java
+++ src/test/java/tests/MarketPageTests.java
@@ -1,63 +1,101 @@
 package tests;
 
 import framework.models.Order;
+import framework.pageObjectModels.CartPage;
 import framework.pageObjectModels.MarketPage;
 import framework.pageObjectModels.OrdersHistoryPage;
+import io.qameta.allure.*;
 import org.testng.annotations.*;
 
-import java.lang.reflect.Method;
-
+@Epic("Positive tests")
+@Feature("Login Tests")
 public class MarketPageTests extends TestBase{
 
     private MarketPage marketPage;
 
-    @BeforeMethod(groups = {"regression", "verifier","updater"})
+    @BeforeClass(groups = {"regression", "verifier","updater"})
     @Parameters({"email","password"})
-    public void openLoginPage(Method method, String email, String password){
+    @Step("Open login page and login with params: email - {0}, password - {1}")
+    public void openLoginPage(String email, String password){
         logger.info("Navigate to login page");
         marketPage = new MarketPage(driver);
-        logger.info(method.getName() + " start");
         logger.info("Login");
         marketPage.doLogin(email,password, driver);
     }
 
-    @AfterMethod(groups = {"regression", "verifier","updater"})
+    @AfterClass(groups = {"regression", "verifier","updater"})
+    @Step("Try to logout")
     public void doLogout(){
         logger.info("Logout");
         marketPage.doLogout();
     }
 
     @DataProvider(name = "orderedUserData")
-    public Object[] getOrders(){
+    public Object[][] getOrders(){
         return fileReader.getData(Order.class);
     }
 
     @Test(dataProvider = "orderedUserData")
+    @Description("Verify that user can open market and add product to cart")
+    @Story("Add product to cart with {0}")
     public void addProductToCartWith(Order order){
 
+        logger.info("Click to WOMEN market button");
         marketPage.womenButtonClick();
 
+        logger.info("Add product ot cart");
         marketPage.addProductToCart(order);
 
-//        marketPage.findProduct(order);
-//        marketPage.waitProductDetails(driver);
-//        marketPage.addToCartButtonClick();
-
+        logger.info("Wait product details");
         marketPage.waitAddedProductInfo(driver);
-        marketPage.compareProdutsName(order,softAssert);
 
-        //marketPage.checkButtonClick();
-       // marketPage.verifyProductName(order.getName(), softAssert);
+        logger.info("Verify product details");
+        marketPage.compareProdutsName(order,softAssert);
 
         softAssert.assertAll();
     }
 
-    @Test(dataProvider = "orderedUserData")
-    public void downloadOrderInfoTest(){
+    @Description("Verify that user can complete order in the cart")
+    @Story("Complete order int the cart")
+    @Test
+    public void completeOrderTest(){
+        CartPage cartPage = new CartPage(driver);
 
+        logger.info("Navigate to shipping");
+        cartPage.openCartPage(driver);
+        cartPage.waitCheckoutButton(driver);
+
+        //Navigate to address confirm
+        cartPage.checkButtonClick();
+
+        //Navigate to shipping
+        cartPage.checkoutAddressButtonClick();
+
+        cartPage.selcetTermsCheckBox();
+        cartPage.checkoutCarrierButtonClick();
+        cartPage.chequeButtonClick();
+        cartPage.submitOrderButtonClick();
+
+        cartPage.successAlertMessageVerify(softAssert);
+
+        softAssert.assertAll();
+    }
+
+    @Description("Verify order in the cart equals order model")
+    @Story("Open user cart and compare order with {0}")
+    @Test(dataProvider = "orderedUserData")
+    public void verifyOrderInCartTest(Order order){
+        CartPage cartPage = new CartPage(driver);
+        cartPage.openCartPage(driver);
+        cartPage.verifyOrder(order, softAssert);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void downloadOrderInfoTest(){
         OrdersHistoryPage historyPage = new OrdersHistoryPage(driver);
 
-        historyPage.verifyFileDownloading(softAssert);
+        historyPage.verifyFileDownloading(softAssert, driver);
 
         softAssert.assertAll();
     }
