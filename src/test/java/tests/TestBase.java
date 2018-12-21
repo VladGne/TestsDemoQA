35e208837fd91dce0e200517b4017493a99951a0
Vlad Gneushev <guinwow@gmail.com>
2018-12-21 18:29:31 +0400

    [+] static context [+] static driver container [+] driver getter

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -11,12 +11,15 @@ import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Listeners(framework.helperClasses.Listener.class)
 
 public class TestBase {
 
-    //ITestContext context;
+    static Map<String, Object> context = new HashMap<>();
+    private static ThreadLocal<WebDriver> storage = new ThreadLocal<>();
+
     SoftAssert softAssert = new SoftAssert();
     protected Logger logger = LogManager.getLogger(this);
     HashMap<String,String> parameters;
@@ -24,11 +27,22 @@ public class TestBase {
     FileReader fileReader;
     int maxFieldLength = 1000;
 
-    @BeforeSuite(groups = {"regression", "contentGenerator", "verifier","updater" })
+    public WebDriver getDriver() {
+        WebDriver current = storage.get();
+
+        if ( null == current ) {
+            logger.debug("Driver for the current thread has not been initialized yet. Creating new Driver." );
+            System.setProperty("webdriver.chrome.driver",BasePage.DRIVER_PATH);
+            current = new ChromeDriver();
+            storage.set( current );
+        }
+        return current;
+    }
+
+    @BeforeSuite
     public void initializeBrowser() {
         logger.info("Browser Initialization");
-        //System.setProperty("webdriver.gecko.driver", BasePage.DRIVER_PATH);
-        System.setProperty("webdriver.chrome.driver",BasePage.DRIVER_PATH);
+        driver = getDriver();
     }
 
     @AfterSuite(groups = {"regression", "verifier","updater", "contentGenerator"})
@@ -39,25 +53,14 @@ public class TestBase {
     @BeforeClass(groups = {"regression", "contentGenerator", "verifier","updater"})
     public void openBasePage(ITestContext testContext){
         logger.info("Open main page");
-        driver = new ChromeDriver();
         parameters = new HashMap<String, String>(testContext.getCurrentXmlTest().getAllParameters());
         fileReader = new FileReader();
         fileReader.processDataFile( parameters.get( "Data" ) );
-       // BasePage basePage = new BasePage(driver);
+    }
+
+    @BeforeMethod
+    public void beforeMethod(ITestContext testContext){
+        this.driver = getDriver();
         testContext.setAttribute("WebDriver", driver);
     }
-
-    @AfterClass
-    public void closeBrowser(){
-        driver.close();
-    }
-
-//    @BeforeMethod
-//    @Parameters({"email","password","login"})
-//    public void doLogin(Boolean login, String email, String password){
-//
-//        if (login){
-//
-//        }
-//    }
 }
